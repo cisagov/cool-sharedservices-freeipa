765857d245e7e14a6c8b3ae8684bdd04da2b535f
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-03-25 15:37:16 -0400

    Dynamically determine the Images account to use
    This is done by determine the account "type" (Production or Staging)
    of the Shared Services account, then looking for the Images account of
    the same type.
    
    Also stop associating public IPs with the FreeIPA master and replica.
    Those instances are in private networks now, so they don't need public
    IPs or public DNS records.

--- freeipa.tf
+++ freeipa.tf
@@ -3,6 +3,31 @@
 #-------------------------------------------------------------------------------
 
 locals {
+  # Get Shared Services account ID from the default provider
+  this_account_id = data.aws_caller_identity.sharedservices.account_id
+
+  # Look up Shared Services account name from AWS organizations
+  # provider
+  this_account_name = [
+    for account in data.aws_organizations_organization.cool.accounts :
+    account.name
+    if account.id == local.this_account_id
+  ][0]
+
+  # Determine Shared Services account type based on account name.
+  #
+  # The account name format is "ACCOUNT_NAME (ACCOUNT_TYPE)" - for
+  # example, "Shared Services (Production)".
+  this_account_type = length(regexall("\\(([^()]*)\\)", local.this_account_name)) == 1 ? regex("\\(([^()]*)\\)", local.this_account_name)[0] : "Unknown"
+
+  # Determine the ID of the corresponding Images account
+  images_account_id = [
+    for account in data.aws_organizations_organization.cool.accounts :
+    account.id
+    if account.name == "Images (${local.this_account_type})"
+  ][0]
+
+  # The subnets where the master and two replicas are to be placed
   master_subnet_cidr   = keys(data.terraform_remote_state.networking.outputs.private_subnets)[0]
   replica1_subnet_cidr = keys(data.terraform_remote_state.networking.outputs.private_subnets)[1]
   replica2_subnet_cidr = keys(data.terraform_remote_state.networking.outputs.private_subnets)[2]
@@ -17,8 +42,8 @@ module "ipa_master" {
   }
 
   admin_pw                    = var.admin_pw
-  ami_owner_account_id        = "207871073513" # The COOL Images account
-  associate_public_ip_address = true
+  ami_owner_account_id        = local.images_account_id
+  associate_public_ip_address = false
   cert_bucket_name            = var.cert_bucket_name
   cert_pw                     = var.master_cert_pw
   cert_read_role_arn          = module.certreadrole_ipa_master.role.arn
@@ -43,8 +68,8 @@ module "ipa_replica1" {
   }
 
   admin_pw                    = var.admin_pw
-  ami_owner_account_id        = "207871073513" # The COOL Images account
-  associate_public_ip_address = true
+  ami_owner_account_id        = local.images_account_id
+  associate_public_ip_address = false
   cert_bucket_name            = var.cert_bucket_name
   cert_pw                     = var.replica1_cert_pw
   cert_read_role_arn          = module.certreadrole_ipa_replica1.role.arn
@@ -67,8 +92,8 @@ module "ipa_replica2" {
   }
 
   admin_pw                    = var.admin_pw
-  ami_owner_account_id        = "207871073513" # The COOL Images account
-  associate_public_ip_address = true
+  ami_owner_account_id        = local.images_account_id
+  associate_public_ip_address = false
   cert_bucket_name            = var.cert_bucket_name
   cert_pw                     = var.replica2_cert_pw
   cert_read_role_arn          = module.certreadrole_ipa_replica2.role.arn
