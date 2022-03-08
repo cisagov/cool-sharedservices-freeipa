35e6d63dbea37e5991232d76f4aab536f5f7692d
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2022-03-08 11:30:31 -0500

    Alphabetize resource parameters

--- dns_records.tf
+++ dns_records.tf
@@ -19,41 +19,34 @@ locals {
 resource "aws_route53_record" "server0_A" {
   provider = aws.sharedservicesprovisionaccount
 
-  zone_id = local.zone_id
   name    = "ipa0.${var.cool_domain}"
-  type    = "A"
+  records = [local.ipa_ips[0]]
   ttl     = var.ttl
-  records = [
-    local.ipa_ips[0],
-  ]
+  type    = "A"
+  zone_id = local.zone_id
 }
 resource "aws_route53_record" "server1_A" {
   provider = aws.sharedservicesprovisionaccount
 
-  zone_id = local.zone_id
   name    = "ipa1.${var.cool_domain}"
-  type    = "A"
+  records = [local.ipa_ips[1]]
   ttl     = var.ttl
-  records = [
-    local.ipa_ips[1],
-  ]
+  type    = "A"
+  zone_id = local.zone_id
 }
 resource "aws_route53_record" "server2_A" {
   provider = aws.sharedservicesprovisionaccount
 
-  zone_id = local.zone_id
   name    = "ipa2.${var.cool_domain}"
-  type    = "A"
+  records = [local.ipa_ips[2]]
   ttl     = var.ttl
-  records = [
-    local.ipa_ips[2],
-  ]
+  type    = "A"
+  zone_id = local.zone_id
 }
 
 resource "aws_route53_record" "server0_PTR" {
   provider = aws.sharedservicesprovisionaccount
 
-  zone_id = local.reverse_zone_ids[0]
   name = format(
     "%s.%s.%s.%s.in-addr.arpa.",
     element(split(".", local.ipa_ips[0]), 3),
@@ -61,16 +54,14 @@ resource "aws_route53_record" "server0_PTR" {
     element(split(".", local.ipa_ips[0]), 1),
     element(split(".", local.ipa_ips[0]), 0),
   )
-  type = "PTR"
-  ttl  = var.ttl
-  records = [
-    "ipa0.${var.cool_domain}",
-  ]
+  records = ["ipa0.${var.cool_domain}"]
+  ttl     = var.ttl
+  type    = "PTR"
+  zone_id = local.reverse_zone_ids[0]
 }
 resource "aws_route53_record" "server1_PTR" {
   provider = aws.sharedservicesprovisionaccount
 
-  zone_id = local.reverse_zone_ids[1]
   name = format(
     "%s.%s.%s.%s.in-addr.arpa.",
     element(split(".", local.ipa_ips[1]), 3),
@@ -78,16 +69,14 @@ resource "aws_route53_record" "server1_PTR" {
     element(split(".", local.ipa_ips[1]), 1),
     element(split(".", local.ipa_ips[1]), 0),
   )
-  type = "PTR"
-  ttl  = var.ttl
-  records = [
-    "ipa1.${var.cool_domain}",
-  ]
+  records = ["ipa1.${var.cool_domain}"]
+  ttl     = var.ttl
+  type    = "PTR"
+  zone_id = local.reverse_zone_ids[1]
 }
 resource "aws_route53_record" "server2_PTR" {
   provider = aws.sharedservicesprovisionaccount
 
-  zone_id = local.reverse_zone_ids[2]
   name = format(
     "%s.%s.%s.%s.in-addr.arpa.",
     element(split(".", local.ipa_ips[2]), 3),
@@ -95,104 +84,101 @@ resource "aws_route53_record" "server2_PTR" {
     element(split(".", local.ipa_ips[2]), 1),
     element(split(".", local.ipa_ips[2]), 0),
   )
-  type = "PTR"
-  ttl  = var.ttl
-  records = [
-    "ipa2.${var.cool_domain}",
-  ]
+  records = ["ipa2.${var.cool_domain}"]
+  ttl     = var.ttl
+  type    = "PTR"
+  zone_id = local.reverse_zone_ids[2]
 }
 
 resource "aws_route53_record" "ca_A" {
   provider = aws.sharedservicesprovisionaccount
 
-  zone_id = local.zone_id
   name    = "ipa-ca.${var.cool_domain}"
-  type    = "A"
-  ttl     = var.ttl
   records = local.ipa_ips
+  ttl     = var.ttl
+  type    = "A"
+  zone_id = local.zone_id
 }
 
 resource "aws_route53_record" "kerberos_TXT" {
   provider = aws.sharedservicesprovisionaccount
 
-  zone_id = local.zone_id
   name    = "_kerberos.${var.cool_domain}"
-  type    = "TXT"
+  records = [upper(var.cool_domain)]
   ttl     = var.ttl
-  records = [
-    upper(var.cool_domain),
-  ]
+  type    = "TXT"
+  zone_id = local.zone_id
 }
 
 resource "aws_route53_record" "master_SRV" {
   for_each = local.tcp_and_udp
   provider = aws.sharedservicesprovisionaccount
 
-  zone_id = local.zone_id
-  name    = "_kerberos-master._${each.value}.${var.cool_domain}"
-  type    = "SRV"
-  ttl     = var.ttl
+  name = "_kerberos-master._${each.value}.${var.cool_domain}"
   records = [
     "0 100 88 ipa0.${var.cool_domain}",
     "0 100 88 ipa1.${var.cool_domain}",
     "0 100 88 ipa2.${var.cool_domain}",
   ]
+  ttl     = var.ttl
+  type    = "SRV"
+  zone_id = local.zone_id
 }
 
 resource "aws_route53_record" "server_SRV" {
   for_each = local.tcp_and_udp
   provider = aws.sharedservicesprovisionaccount
 
-  zone_id = local.zone_id
-  name    = "_kerberos._${each.value}.${var.cool_domain}"
-  type    = "SRV"
-  ttl     = var.ttl
+  name = "_kerberos._${each.value}.${var.cool_domain}"
   records = [
     "0 100 88 ipa0.${var.cool_domain}",
     "0 100 88 ipa1.${var.cool_domain}",
     "0 100 88 ipa2.${var.cool_domain}",
   ]
+  ttl     = var.ttl
+  type    = "SRV"
+  zone_id = local.zone_id
 }
 
 resource "aws_route53_record" "password_SRV" {
   for_each = local.tcp_and_udp
   provider = aws.sharedservicesprovisionaccount
 
-  zone_id = local.zone_id
-  name    = "_kpasswd._${each.value}.${var.cool_domain}"
-  type    = "SRV"
-  ttl     = var.ttl
+  name = "_kpasswd._${each.value}.${var.cool_domain}"
   records = [
     "0 100 464 ipa0.${var.cool_domain}",
     "0 100 464 ipa1.${var.cool_domain}",
     "0 100 464 ipa2.${var.cool_domain}",
   ]
+  ttl     = var.ttl
+  type    = "SRV"
+  zone_id = local.zone_id
 }
 
 resource "aws_route53_record" "ldap_SRV" {
   provider = aws.sharedservicesprovisionaccount
 
-  zone_id = local.zone_id
-  name    = "_ldap._tcp.${var.cool_domain}"
-  type    = "SRV"
-  ttl     = var.ttl
+  name = "_ldap._tcp.${var.cool_domain}"
   records = [
     "0 100 389 ipa0.${var.cool_domain}",
     "0 100 389 ipa1.${var.cool_domain}",
     "0 100 389 ipa2.${var.cool_domain}",
   ]
+  ttl     = var.ttl
+  type    = "SRV"
+  zone_id = local.zone_id
 }
 
 resource "aws_route53_record" "ldaps_SRV" {
   provider = aws.sharedservicesprovisionaccount
 
-  zone_id = local.zone_id
-  name    = "_ldaps._tcp.${var.cool_domain}"
-  type    = "SRV"
-  ttl     = var.ttl
+  name = "_ldaps._tcp.${var.cool_domain}"
   records = [
     "0 100 636 ipa0.${var.cool_domain}",
     "0 100 636 ipa1.${var.cool_domain}",
     "0 100 636 ipa2.${var.cool_domain}",
   ]
+  ttl     = var.ttl
+  type    = "SRV"
+  zone_id = local.zone_id
 }
